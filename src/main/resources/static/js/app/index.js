7b8e87565f5373c60323affa9e1f653cac3c213e
ADMIN <bow7553@gmail.com>
2021-08-04 17:29:38 +0900

    140p

---
+++ src/main/resources/static/js/app/index.js
@@ -0,0 +1,17 @@
+var main = {
+    init : function () {
+        var _this = this;
+        $('#btn-save').on('click', function () {
+            _this.save();
+        });
+    },
+    save : function () {
+        var data = {
+            title : $('#title').val(),
+            author : $('#author').val(),
+            content : $('#content').val()
+        };
+
+
+    }
+}
